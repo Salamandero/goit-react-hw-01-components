0994bb7d044dd1f5d127ae67fef4fd8095aed1ae
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-12-08 13:45:51 +0200

    render 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+//for painting.js
+
+import user from './jsonData/user.json';
+ 
+import { Profile } from "./Profile/Profile";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile   
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}/>
+
     </div>
   );
 };
+
