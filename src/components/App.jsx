c89d1895a0332129de1ccb9805b67d821f52bdfd
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2022-12-12 21:14:00 +0200

    add style for Profile.module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,18 @@ import user from './jsonData/user.json';
 import Profile from "./Profile/Profile";
 import Statistics from './Statistics/Statistics';
 import data  from './jsonData/data.json';
-import FriendsList from './FriendList/FriendsList';
+import FriendsList from './FriendsList/FriendsList';
 import friends from "./jsonData/friends.json";
 import TransactionHistory from './Transactions/TransactionHistory';
 import transactions from "./jsonData/transactions.json";
-
+import css from "./App.module.css";
 
 import React from 'react';
-
 
 export const App = () => {
   return (
  
-       <div>
+       <div className={css.wrapper}>
        <Profile   
   username={user.username}
   tag={user.tag}
